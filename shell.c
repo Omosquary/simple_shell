94db2f34ee44364e7bd5ce1a69b90c8f386a6d81
Omosquary <iamomosquare@gmail.com>
2022-06-10 03:11:41 -0700

    Trial

---
+++ shell.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+/**
+ * main - is a function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *buffer;
+	size_t length = 1024;
+	char *token;
+	int read;
+
+	buffer = malloc(sizeof(size_t) * length);
+	while (1)
+	{
+		printf("cisfun$ ");
+		read = getline(&buffer, &length, stdin);
+		if (read != EOF)
+		{
+			printf("buffer getline: %s", buffer);
+		}
+
+		token = strtok(buffer, " ");
+		while (token != NULL)
+		{
+			printf("buffer strtok: %s\n", token);
+			token = strtok(NULL, " ");
+		}
+	}
+	return (0);
+}
